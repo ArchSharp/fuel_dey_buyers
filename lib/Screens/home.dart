e70e437f0e2d8aa2221448d42a82249944c30d6f
ayodeji.olalekan <ayodeji.olalekan@konga.com>
2024-06-21 04:00:52 +0100

    added geolocator and geocoding to get users coordinate address

--- lib/Screens/home.dart
+++ lib/Screens/home.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -9,6 +13,96 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  Position? _currentPosition;
+  bool? _hasPermission;
+  String? _address;
+
+  @override
+  void initState() {
+    super.initState();
+    _checkPermission();
+  }
+
+  Future<void> _checkPermission() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool? permissionGranted = prefs.getBool('location_permission_granted');
+
+    if (permissionGranted == null) {
+      // First time asking for permission
+      _showPermissionDialog();
+    } else {
+      setState(() {
+        _hasPermission = permissionGranted;
+      });
+      if (permissionGranted) {
+        _getCurrentLocation();
+      }
+    }
+  }
+
+  void _showPermissionDialog() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('Allow Location Access'),
+        content: const Text('Do you want to allow location access?'),
+        actions: [
+          TextButton(
+            onPressed: () => _onPermissionDenied(),
+            child: const Text('No'),
+          ),
+          TextButton(
+            onPressed: () => _onPermissionGranted(),
+            child: const Text('Yes'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _onPermissionDenied() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('location_permission_granted', false);
+    setState(() {
+      _hasPermission = false;
+    });
+    Navigator.of(context).pop();
+  }
+
+  void _onPermissionGranted() async {
+    PermissionStatus permissionStatus = await Permission.location.request();
+    bool isGranted = permissionStatus == PermissionStatus.granted;
+
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('location_permission_granted', isGranted);
+
+    setState(() {
+      _hasPermission = isGranted;
+    });
+
+    Navigator.of(context).pop();
+
+    if (isGranted) {
+      _getCurrentLocation();
+    }
+  }
+
+  Future<void> _getCurrentLocation() async {
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    List<Placemark> placemarks =
+        await placemarkFromCoordinates(position.latitude, position.longitude);
+    String address =
+        "${placemarks[0].postalCode} ${placemarks[0].street} ${placemarks[0].street} ${placemarks[0].locality} ${placemarks[0].country}";
+
+    //print("Placemarks: " + placemarks.toString());
+
+    setState(() {
+      _currentPosition = position;
+      _address = address;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     // double deviceWidth = MediaQuery.of(context).size.width;
@@ -23,10 +117,10 @@ class _HomeState extends State<Home> {
           child: Column(
             children: <Widget>[
               SizedBox(height: mtop),
-              const Center(
+              Center(
                 child: Text(
-                  "Welcome to Fuel Dey",
-                  style: TextStyle(color: Colors.green, fontSize: 20),
+                  "Welcome to Fuel Dey Latitude: ${_currentPosition?.latitude}, Longitude: ${_currentPosition?.longitude}, Address: ${_address}",
+                  style: const TextStyle(color: Colors.green, fontSize: 20),
                 ),
               ),
             ],
