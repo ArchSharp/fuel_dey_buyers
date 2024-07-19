33d4944edca0882620177d2c4cabbba628f6bc25
ayodeji.olalekan <ayodeji.olalekan@konga.com>
2024-07-19 13:32:21 +0100

    added botto navigation bar

---
+++ lib/Screens/SupportingScreens/bottom_navbar.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class BottomNavbar extends StatefulWidget {
+  final ValueChanged<int> onIndexChanged;
+
+  const BottomNavbar({
+    super.key,
+    required this.onIndexChanged,
+  });
+
+  @override
+  State<BottomNavbar> createState() => _BottomNavbarState();
+}
+
+class _BottomNavbarState extends State<BottomNavbar> {
+  int _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
+      currentIndex: _currentIndex,
+      items: <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(
+            _currentIndex == 0 ? Icons.home : Icons.home_outlined,
+            color: _currentIndex == 0 ? const Color(0xFF2C2D2F) : Colors.grey,
+          ),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(
+            _currentIndex == 1
+                ? Icons.bookmark_sharp
+                : Icons.bookmark_border_sharp,
+            color: _currentIndex == 1 ? const Color(0xFF2C2D2F) : Colors.grey,
+          ),
+          label: 'Favorite',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(
+            _currentIndex == 2
+                ? Icons.notifications
+                : Icons.notifications_outlined,
+            color: _currentIndex == 2 ? const Color(0xFF2C2D2F) : Colors.grey,
+          ),
+          label: 'Notification',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(
+            _currentIndex == 3 ? Icons.settings_sharp : Icons.settings_outlined,
+            color: _currentIndex == 3 ? const Color(0xFF2C2D2F) : Colors.grey,
+          ),
+          label: 'Setting',
+        ),
+      ],
+      selectedLabelStyle: const TextStyle(
+          color: Color(0xFF2C2D2F), fontSize: 10, fontWeight: FontWeight.w600),
+      unselectedLabelStyle: const TextStyle(
+          color: Color(0xFF2C2D2F), fontSize: 10, fontWeight: FontWeight.w600),
+      onTap: (index) {
+        setState(() {
+          _currentIndex = index;
+        });
+        widget.onIndexChanged(index);
+      },
+    );
+  }
+}
